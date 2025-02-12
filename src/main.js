c367575b57b85d140e37e7e5ec98214e1ffc062b
Rebecca-Peng <pengf603@newschool.edu>
2025-02-11 20:34:50 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import{
 }from '@snap/camera-kit'
 
 (async function (){
-    var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM0OTg0ODAwLCJzdWIiOiIyMDVhYTNlMy0zZGIxLTQyOWUtOTg1My0wODc2ZDcxYmZiZTl-U1RBR0lOR344ZmY4NTY1ZC1lODFjLTQxNDItYTlhYi1hMmY4ODE3MTUxNmIifQ.IFchVq8Yo7W0YHDn_qPz0H5MHAHEZIjE1FC2YaTLYVc'})
+    var cameraKit = await bootstrapCameraKit({ apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzM0OTg0ODAwLCJzdWIiOiIyMDVhYTNlMy0zZGIxLTQyOWUtOTg1My0wODc2ZDcxYmZiZTl-UFJPRFVDVElPTn5hOGMzOGU0OC02MDNjLTRmYTItYTlmOS0wNDc5MTg3YjYxNDYifQ.IAmIuzW8KdOlp3NBJu2kCSGIDbMnBFco2Uwy4b_rLjY'})
 
     const session = await cameraKit.createSession()
     document.getElementById('canvas').replaceWith(session.output.live)
